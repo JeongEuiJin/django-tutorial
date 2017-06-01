3f7e9b9493cba75d550e66504cd4737e3d970818
JeongEuiJin <hehar1020@gmail.com>
2017-06-01 12:28:21 +0900

    setting , langugae, timezone change

--- django-app/mysite/settings.py
+++ django-app/mysite/settings.py
@@ -103,10 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'ko-kr'
+#langecode 한국
+TIME_ZONE = 'Asia/Seoul'
+#timezone 아시아/서울
 USE_I18N = True
 
 USE_L10N = True
