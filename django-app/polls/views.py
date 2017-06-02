7a89b1b343d70535a161cdd00063652d7425ef91
JeongEuiJin <hehar1020@gmail.com>
2017-06-02 12:43:20 +0900

    add namespace to polls/urls.py,use url tag in template

--- django-app/polls/views.py
+++ django-app/polls/views.py
@@ -1,13 +1,13 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
 
 from polls.models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = get_list_or_404(Question.objects.order_by('-pub_date')[:5])
     context = {
         'latest_question_list': latest_question_list,
     }
@@ -16,10 +16,13 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoseNotExist as e:
-        raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
+    #question.choice_set.
+    #Choice.objects.filter(question=question) 두개는 같은 문구이다
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoseNotExist as e:
+    #     raise Http404('Question does not exist')
     context = {
         'question': question
     }
